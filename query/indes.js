227cb0638831b73bc1e762c2bfb2c70a06371026
ArslanRasheed60 <arslanrashee1@gmail.com>
2023-06-01 14:13:57 +0500

    Updated: Query service

--- query/indes.js
+++ query/indes.js
@@ -6,8 +6,30 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/post", (req, res) => {});
+const posts = {};
 
-app.post("/events", (req, res) => {});
+app.get("/post", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { postId, id, content } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+
+  res.send({});
+});
 
 app.listen(4002, () => console.log("Listening to port 4002"));
