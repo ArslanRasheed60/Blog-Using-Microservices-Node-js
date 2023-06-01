fa276fdaeb7ddcf81e0a582525f8465086f37cfb
ArslanRasheed60 <arslanrashee1@gmail.com>
2023-06-01 16:21:08 +0500

    Added: moderate service

---
+++ moderate/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {});
+
+app.listen(4003, () => console.log(" (moderate) Listening on 4003"));
